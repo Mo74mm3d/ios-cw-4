63ce614134cb341001bb34a2807fef1e02804068
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-05 13:13:49 +0300

    Done

---
+++ ios-cw-4.playground/Contents.swift
@@ -0,0 +1,5 @@
+let name:String = "Mohammed"
+let hobby:String = "gaming"
+let favoriteNumber:Int = 8
+
+print("Hello my name is \(name) and my favorite number is \(favoriteNumber) and my favorite hobby is \(hobby).")
